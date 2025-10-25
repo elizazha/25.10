7960994d2ee13eccc89a3d4df273694a222e8b13
elizazha <lizokbella@mail.ru>
2025-10-25 19:33:24 +0300

    last+new

--- main.cpp
+++ main.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <limits>
 bool isPyth(unsigned a, unsigned b, unsigned c) {
 	bool p = (a * a == (b * b + c * c));
 	p = p || (b*b == (a * a + c * c));
 	p = p || (c * c == (a * a + b * b));
 	return p;
 }
+unsigned max() {
+  return std::numeric_limits<unsigned>::max();
+}
 
-int main4() {
+int main() {
 	using u_t = unsigned;
 	///u_t a = 0; -- 1-2)
 	u_t a = 0, b = 0, c = 0;
@@ -15,10 +19,17 @@ int main4() {
 	//if (std::cin) {
 	//	count++;
 	//}
+	std::cin>>c>>b;
 	while (std::cin >> a) {
 		count += isPyth(a, b, c) ? 1 : 0;
 		c = b;
 		b = a;
+		if ((max()/a<a)||(max()/b<b)||(max()/c<c)){
+		    std::cerr << "error 2\n";
+                        return 1;}
+		if ((max()-a*a<b*b)||(max()-a*a<c*c)||(max()-b*b<c*c)){std::cerr << "error 2\n";
+                        return 1;}
+
 		// count ++; -- 1)
 		//count+=a*a;--2)
 	}
@@ -27,7 +38,7 @@ int main4() {
 		std::cout << count << "\n";
 	}
 	else if (std::cin.fail()) {
-			std::cerr << "error\n";
+			std::cerr << "error 1\n";
 			return 1;
 		}
 }
